7101602f6717f00839342dbdca424d073cd3bb31
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-04 20:23:29 +0200

    REFACTOR: class name

--- components/MHeader.vue
+++ components/MHeader.vue
@@ -15,9 +15,9 @@
             key="image"
             src="/images/MihailoPantovic.jpeg"
             alt="Mihailo Pantovic"
-            class="items image rounded-full border-white border-4 w-32 h-32"
+            class="animated image rounded-full border-white border-4 w-32 h-32"
           />
-          <ul key="social" class="flex items social">
+          <ul key="social" class="flex animated social">
             <SocialIcon link="https://github.com/Mihailo23">
               <path
                 d="M10.015 9.949h-.03c-1.191 0-2.24-.303-2.861.268a1.57 1.57 0 0 0-.527 1.197c0 1.852 1.483 2.08 3.389 2.08h.029c1.905 0 3.389-.229 3.389-2.08 0-.443-.156-.856-.527-1.197-.622-.571-1.671-.268-2.862-.268zM8.393 12.48c-.363 0-.656-.408-.656-.91s.293-.908.656-.908c.363 0 .657.406.657.908.001.502-.293.91-.657.91zm3.213 0c-.363 0-.657-.408-.657-.91s.294-.908.657-.908c.362 0 .656.406.656.908.001.502-.293.91-.656.91zM10 .4C4.698.4.4 4.698.4 10s4.298 9.6 9.6 9.6 9.6-4.298 9.6-9.6S15.302.4 10 .4zm.876 13.539c-.172 0-.514 0-.876.002-.362-.002-.704-.002-.876-.002-.76 0-3.772-.059-3.772-3.689 0-.834.286-1.445.755-1.955-.074-.184-.078-1.232.32-2.236 0 0 .916.1 2.301 1.051.289-.081.781-.122 1.272-.122s.982.041 1.273.121c1.385-.951 2.301-1.051 2.301-1.051.398 1.004.395 2.053.32 2.236.469.51.755 1.121.755 1.955-.001 3.632-3.013 3.69-3.773 3.69z"
@@ -33,7 +33,7 @@
           </ul>
           <ul
             key="projects"
-            class="flex text-white items projects transition duration-500"
+            class="flex text-white animated projects transition duration-500"
           >
             <ProjectBubble project="dev9t" />
             <ProjectBubble project="asteria-led" />
@@ -112,7 +112,7 @@ header {
   z-index: 1000;
   //   background: rgba(15, 19, 20, 0.45);
 }
-.items,
+.animated,
 .list-move,
 .social a,
 .projects a {
