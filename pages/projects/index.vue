8aad93461bb9c9590624f5adfaadfedad9c9f11c
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-05 08:04:39 +0200

    ADD: projects translations

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,36 +1,27 @@
 <template>
   <div class="container px-2 sm:px-0">
     <div class="my-4">
-      <h1 class="text-blue-500 text-6xl font-bold leading-none">
-        Projects
-      </h1>
+      <h1
+        class="text-blue-500 text-6xl font-bold leading-none"
+        v-text="$t('projects.title')"
+      />
     </div>
     <div class="flex flex-wrap -mx-2">
       <div class="w-full sm:w-2/3 px-2">
         <main>
+          <m-card :title="$t('projects.dev9t.title')" link="http://dev9t.com" />
           <m-card
-            title="dev9t.com— website for a Belgrade festival"
-            link="http://dev9t.com"
-          >
-          </m-card>
-
-          <m-card
-            title="osbrankopesic.edu.rs — website for a school in Belgrade"
+            :title="$t('projects.branko-pesic.title')"
             link="https://osbrankopesic.edu.rs"
-          >
-          </m-card>
-
+          />
           <m-card
-            title="asteria-led.com — website for Croatian LED company"
+            :title="$t('projects.asteria-led.title')"
             link="http://asteria-led.com"
-          >
-          </m-card>
-
+          />
           <m-card
-            title="asteria-adventures.com — website for Croatian ATV company"
+            :title="$t('projects.asteria-adventures.title')"
             link="http://asteria-adventures.com"
-          >
-          </m-card>
+          />
         </main>
       </div>
       <div class="w-full sm:w-1/3 px-2">
