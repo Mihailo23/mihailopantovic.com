fbd896c7ebf6d38ce4f022b67099d39deab2c115
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-05 16:20:23 +0200

    FIX: sidebar alignment

--- components/MSidebar.vue
+++ components/MSidebar.vue
@@ -1,9 +1,9 @@
 <template>
-  <aside>
+  <aside class="text-center sm:text-left">
     <div class="card">
       <hr class="my-4" />
       <h5 class="text-blue-500 font-bold" v-text="$t('sidebar.languages')" />
-      <div class="flex">
+      <div class="flex justify-center sm:justify-start">
         <MTag name="javascript" />
         <MTag name="html5" />
         <MTag name="php" />
@@ -14,7 +14,7 @@
     <div class="card">
       <hr class="my-4" />
       <h5 class="text-blue-500 font-bold" v-text="$t('sidebar.databases')" />
-      <div class="flex">
+      <div class="flex justify-center sm:justify-start">
         <MTag name="mysql" />
         <MTag name="mongodb" />
       </div>
@@ -23,12 +23,12 @@
       <hr class="my-4" />
       <h5 class="text-blue-500 font-bold" v-text="$t('sidebar.frameworks')" />
       <h6 class="mt-2 font-bold text-sm">PHP:</h6>
-      <div class="flex mb-4">
+      <div class="flex justify-center sm:justify-start mb-4">
         <MTag name="wordpress" />
         <MTag name="laravel" />
       </div>
       <h6 class="mt-2 font-bold text-sm">FRONT-END:</h6>
-      <div class="flex mb-4">
+      <div class="flex justify-center sm:justify-start mb-4">
         <MTag name="vuejs" />
         <MTag name="jquery" />
         <MTag name="bootstrap" />
@@ -37,7 +37,7 @@
         <!-- <MTag name="tailwind" /> -->
       </div>
       <h6 class="mt-2 font-bold text-sm">MOBILE:</h6>
-      <div class="flex">
+      <div class="flex justify-center sm:justify-start">
         <MTag name="react" />
       </div>
     </div>
@@ -48,18 +48,18 @@
         class="mt-2 font-bold text-sm"
         v-text="$t('sidebar.version-control')"
       />
-      <div class="flex mb-4">
+      <div class="flex justify-center sm:justify-start mb-4">
         <MTag name="git" />
       </div>
       <h6 class="mt-2 font-bold text-sm" v-text="$t('sidebar.ide')" />
-      <div class="flex mb-4">
+      <div class="flex justify-center sm:justify-start mb-4">
         <MTag name="visualstudio" />
         <MTag name="phpstorm" />
         <!-- <MTag name="sublime" /> -->
         <MTag name="photoshop" />
       </div>
       <h6 class="mt-2 font-bold text-sm" v-text="$t('sidebar.environments')" />
-      <div class="flex">
+      <div class="flex justify-center sm:justify-start">
         <MTag name="linux" />
         <MTag name="apple" />
         <MTag name="windows8" />
