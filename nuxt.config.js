ed6e49482d30321b540c3b9bd5ee098fc82dc11e
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-07 11:30:28 +0200

    ADD: preload font

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,8 +76,9 @@ export default {
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Roboto&display=swap"
+        rel: "preload",
+        href: "https://fonts.googleapis.com/css?family=Roboto&display=swap",
+        onLoad: "this.onload=null;this.rel='stylesheet'"
       },
       {
         rel: "preload",
