1b660d7498c983521e902c19fbb170644365e4a6
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-17 19:47:31 +0200

    ADD: animated nav links

--- components/navigation/NavigationItem.vue
+++ components/navigation/NavigationItem.vue
@@ -2,7 +2,7 @@
   <nuxt-link
     :exact="exact"
     :to="to"
-    class="outline-none transition duration-500 uppercase text-xs font-bold tracking-widest px-3 py-1 border-b-4 border-transparent hover:border-black focus:border-black"
+    class="link outline-none transition duration-500 uppercase text-xs font-bold tracking-widest px-3 py-1"
   >
     <slot
   /></nuxt-link>
@@ -21,7 +21,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nuxt-link-active {
-  border-color: #000000;
+.link {
+  position: relative;
+}
+.link::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 100%;
+  border-top: 0.25rem solid black;
+  border-radius: 0.25rem;
+  transition: right 0.5s cubic-bezier(0, 0.5, 0, 1);
+}
+.link:hover::after {
+  right: 0;
+}
+.nuxt-link-active::after {
+  right: 0;
 }
 </style>
