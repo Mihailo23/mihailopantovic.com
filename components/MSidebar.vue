9217d78a6147a5428d6f6a805c02b121fc3a2085
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-18 05:04:13 +0200

    ADD: icon tooltips

--- components/MSidebar.vue
+++ components/MSidebar.vue
@@ -4,10 +4,10 @@
       <hr class="my-4" />
       <MH5 v-text="$t('sidebar.languages')" />
       <div class="flex justify-center sm:justify-end">
-        <MTag name="javascript" />
-        <MTag name="html5" />
-        <MTag name="php" />
-        <MTag name="css3" />
+        <MTag icon="javascript" title="javascript" />
+        <MTag icon="html5" title="html5" />
+        <MTag icon="php" title="php" />
+        <MTag icon="css3" title="css3" />
       </div>
     </div>
 
@@ -15,8 +15,8 @@
       <hr class="my-4" />
       <MH5 v-text="$t('sidebar.databases')" />
       <div class="flex justify-center sm:justify-end">
-        <MTag name="mysql" />
-        <MTag name="mongodb" />
+        <MTag icon="mysql" title="mysql" />
+        <MTag icon="mongodb" title="mongodb" />
       </div>
     </div>
     <div class="card">
@@ -24,21 +24,20 @@
       <MH5 v-text="$t('sidebar.frameworks')" />
       <h6 class="mt-2 text-sm">PHP:</h6>
       <div class="flex justify-center sm:justify-end mb-4">
-        <MTag name="wordpress" />
-        <MTag name="laravel" />
+        <MTag icon="wordpress" title="wordpress" />
+        <MTag icon="laravel" title="laravel" />
       </div>
       <h6 class="mt-2 text-sm">FRONT-END:</h6>
       <div class="flex justify-center sm:justify-end mb-4">
-        <MTag name="vuejs" />
-        <MTag name="jquery" />
-        <MTag name="bootstrap" />
-        <MTag name="react" />
-        <MTag name="sass" />
-        <!-- <MTag name="tailwind" /> -->
+        <MTag icon="vuejs" title="vuejs" />
+        <MTag icon="jquery" title="jquery" />
+        <MTag icon="bootstrap" title="bootstrap" />
+        <MTag icon="react" title="react" />
+        <MTag icon="sass" title="sass" />
       </div>
       <h6 class="mt-2 text-sm">MOBILE:</h6>
       <div class="flex justify-center sm:justify-end">
-        <MTag name="react" />
+        <MTag icon="react" title="react" />
       </div>
     </div>
     <div class="card">
@@ -46,20 +45,19 @@
       <MH5 v-text="$t('sidebar.other')" />
       <h6 class="mt-2 text-sm" v-text="$t('sidebar.version-control')" />
       <div class="flex justify-center sm:justify-end mb-4">
-        <MTag name="git" />
+        <MTag icon="git" title="git" />
       </div>
       <h6 class="mt-2 text-sm" v-text="$t('sidebar.ide')" />
       <div class="flex justify-center sm:justify-end mb-4">
-        <MTag name="visualstudio" />
-        <MTag name="phpstorm" />
-        <!-- <MTag name="sublime" /> -->
-        <MTag name="photoshop" />
+        <MTag icon="visualstudio" title="visualstudio" />
+        <MTag icon="phpstorm" title="phpstorm" />
+        <MTag icon="photoshop" title="photoshop" />
       </div>
       <h6 class="mt-2 text-sm" v-text="$t('sidebar.environments')" />
       <div class="flex justify-center sm:justify-end">
-        <MTag name="linux" />
-        <MTag name="apple" />
-        <MTag name="windows8" />
+        <MTag icon="linux" title="linux" />
+        <MTag icon="apple" title="apple" />
+        <MTag icon="windows8" title="windows8" />
       </div>
     </div>
   </aside>
--- components/UI/MTag.vue
+++ components/UI/MTag.vue
@@ -1,16 +1,22 @@
 <template>
   <span
-    class="flex items-center justify-center w-10 h-10 rounded-full leading-none"
-    :title="`${name.toUpperCase()}`"
+    class=" flex items-center justify-center w-10 h-10 rounded-full leading-none"
   >
-    <i :class="`devicon-${name}-plain colored text-2xl`"></i>
+    <i
+      :class="`icon devicon-${icon}-plain colored text-2xl`"
+      :data-title="title"
+    ></i>
   </span>
 </template>
 
 <script>
 export default {
   props: {
-    name: {
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
       type: String,
       required: true
     }
@@ -18,4 +24,32 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.icon {
+  position: relative;
+}
+.icon::after {
+  font-family: Roboto;
+  display: block;
+  content: attr(data-title);
+  background-color: #ffffff;
+  border: 1px solid;
+  border-radius: 0.25rem;
+  color: inherit;
+  padding: 0.25rem;
+  font-size: 0.75rem;
+  position: absolute;
+  z-index: 1;
+  top: -3rem;
+  left: 50%;
+  transform: translateX(-50%);
+  opacity: 0;
+  transition: all 0.5s cubic-bezier(0, 0.5, 0, 1);
+}
+.icon:hover::after {
+  opacity: 1;
+}
+.icon:hover::after {
+  top: -2rem;
+}
+</style>
