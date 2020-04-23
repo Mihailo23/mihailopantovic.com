e1e2757a6de3da08b2d997ce7c069b11f18cd574
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-04-23 08:49:49 +0200

    ADD: testimonial

---
+++ components/UI/MTestimonial.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    class="quote-wrapper relative"
+    :style="{
+      height: '300px',
+      width: '300px'
+    }"
+  >
+    <blockquote
+      class="text text-gray-100 text-shadow relative h-full w-full"
+      cite="http://www.inspireux.com/category/quotes/jesse-james-garrett/"
+    >
+      <p class="text-center h-full">
+        <span class="italic" v-text="$t(project.testimonial)" />
+      </p>
+      <p
+        class="absolute bottom-0 mb-8 text-center w-full"
+        v-text="project.owner"
+      />
+    </blockquote>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.text {
+  background: radial-gradient(
+    ellipse at center,
+    black 0%,
+    black 70%,
+    transparent 70.3%
+  );
+}
+.text::before {
+  content: "";
+  float: left;
+  height: 100%;
+  width: 50%;
+  shape-outside: polygon(
+    0 0,
+    0 100%,
+    100% 100%,
+    60% 95%,
+    25% 85%,
+    10% 70%,
+    0 50%,
+    10% 30%,
+    25% 15%,
+    60% 5%,
+    100% 0
+  );
+  shape-margin: 7%;
+}
+.text p {
+  &::before {
+    content: "";
+    float: right;
+    height: 100%;
+    width: 50%;
+    shape-outside: polygon(
+      0 0,
+      100% 0,
+      100% 100%,
+      0 100%,
+      40% 95%,
+      75% 85%,
+      90% 70%,
+      100% 50%,
+      90% 30%,
+      75% 15%,
+      40% 5%
+    );
+    shape-margin: 7%;
+  }
+}
+.quote-wrapper::before {
+  content: "\201C";
+  color: #ccc;
+  font-family: sans-serif, serif;
+  font-size: 270px;
+  height: 82px;
+  line-height: 1;
+  opacity: 0.9;
+  position: absolute;
+  top: -3rem;
+  left: 0;
+  z-index: 1;
+}
+</style>
--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,34 +1,28 @@
 <template>
   <div class="container px-2 sm:px-0">
-    <h1
-      class="my-8 text-brand text-4xl sm:text-5xl font-bold leading-none capitalize"
-      v-text="title"
-    />
-    <h2 class="text-6xl">TODO</h2>
+    <div class="flex">
+      <h1
+        class="md:mt-32 my-8 text-brand text-4xl sm:text-5xl font-bold leading-none capitalize"
+        v-text="$t(project.name)"
+      />
+    </div>
+    <div class="flex items-center justify-center">
+      <m-testimonial v-if="project.testimonial" :project="project" />
+    </div>
   </div>
 </template>
 
 <script>
+import projects from "~/assets/projects.json";
+import MTestimonial from "~/components/UI/MTestimonial";
 export default {
-  data() {
-    return {
-      project: ""
-    };
+  components: {
+    MTestimonial
   },
   computed: {
-    title() {
-      return this.project.split("-").join(" ");
-    }
-  },
-  watch: {
-    "$route.params.project": {
-      handler(value) {
-        this.project = value;
-      },
-      immediate: true
+    project() {
+      return projects[this.$route.params.project];
     }
   }
 };
 </script>
-
-<style lang="scss" scoped></style>
