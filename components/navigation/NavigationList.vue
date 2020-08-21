ec82080a6f608aa2a592c06964ed184096f3c080
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-08-21 12:10:10 +0200

    TEST: is language picker the issue?

--- components/navigation/NavigationList.vue
+++ components/navigation/NavigationList.vue
@@ -1,13 +1,23 @@
 <template>
   <ul class="flex text-gray-800 items-center h-10 py-1">
     <li>
-      <NavigationItem to="/" exact class="mr-2" :text="$t('links.home')" />
+      <NavigationItem
+        to="/"
+        exact
+        class="mr-2"
+        :text="$t('links.home')"
+      />
     </li>
     <li>
-      <NavigationItem to="/projects" :text="$t('links.projects')" />
+      <NavigationItem
+        to="/projects"
+        :text="$t('links.projects')"
+      />
     </li>
     <li class="ml-auto">
-      <LanguageChanger />
+      <client-only>
+        <LanguageChanger />
+      </client-only>
     </li>
   </ul>
 </template>
