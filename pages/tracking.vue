7572f37a9d758bec36c2ed0617e9959a97345adb
Mihailo Pantovic <mihailo@snappykraken.com>
2023-09-28 16:11:31 +0200

    UPDATE

--- pages/tracking.vue
+++ pages/tracking.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container pt-24">
     <div v-if="isGazeRecorderApiLoaded">
-      <button @click="stopTracking">stop tracking</button>
+      <button @click="startTracking">Start tracking</button>
+      <button @click="stopTracking">Stop tracking</button>
       <button @click="replay" v-if="SesionReplayData">replay</button>
     </div>
   </div>
@@ -10,42 +11,38 @@
 export default {
   data() {
     return {
-      isGazeRecorderApiLoaded: false,
       SesionReplayData: null
     }
   },
   head () {
     return {
       script: [
-      {
+        {
           hid: 'gaze-recorder',
           src: 'https://app.gazerecorder.com/GazeRecorderAPI.js',
-          // Changed after script load
-          callback: () => { this.isGazeRecorderApiLoaded = true } 
+          defer: true,
         },
         {
           hid: 'gaze-replay',
           src: 'https://app.gazerecorder.com/GazePlayer.js',
+          defer: true
         }
       ]
     }
   },
   methods: {
+    startTracking() {
+      window.GazeRecorderAPI.Rec();
+      console.log('started');
+    },
     stopTracking() {
-      window.GazeRecorderAPI.Rec()
+      window.GazeRecorderAPI.StopRec();
       this.SesionReplayData = window.GazeRecorderAPI.GetRecData();
+      console.log('stopped');
     },
     replay() {
       window.GazePlayer.PlayResultsData(this.SesionReplayData );
-
     }
   },
-  watch: {
-    isGazeRecorderApiLoaded(value) {
-      if(value) {
-        window.GazeRecorderAPI.StopRec();
-      }
-    }
-  }
 }
 </script>
