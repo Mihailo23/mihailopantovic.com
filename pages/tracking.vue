02f87d5ea3851cb68a99b539e9d6cbbf4a752110
Mihailo Pantovic <mihailo@snappykraken.com>
2023-09-29 09:41:44 +0200

    TEST

--- pages/tracking.vue
+++ pages/tracking.vue
@@ -1,46 +1,73 @@
 <template>
-  <div class="container pt-24">
-    <button @click="startTracking">Start tracking</button>
-    <button @click="stopTracking">Stop tracking</button>
-    <button @click="replay" v-if="SesionReplayData">replay</button>
+  <div class="container pt-40">
+    <div v-if="isGazeCloudApiLoaded">
+      <button @click="stopTracking">stop tracking</button>
+    </div>
+    <div id="gaze"
+        style='position: absolute;display:none;width: 100px;height: 100px;border-radius: 50%;border: solid 2px  rgba(255, 255,255, .2);	box-shadow: 0 0 100px 3px rgba(125, 125,125, .5);	pointer-events: none;	z-index: 999999'>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      SesionReplayData: null
+      isGazeCloudApiLoaded: false
     }
   },
   head () {
     return {
       script: [
         {
-          hid: 'gaze-recorder',
-          src: 'https://app.gazerecorder.com/GazeRecorderAPI.js',
+          hid: 'gaze',
+          src: 'https://api.gazerecorder.com/GazeCloudAPI.js',
           defer: true,
-        },
-        {
-          hid: 'gaze-replay',
-          src: 'https://app.gazerecorder.com/GazePlayer.js',
-          defer: true
+          // Changed after script load
+          callback: () => { this.isGazeCloudApiLoaded = true } 
         }
       ]
     }
   },
   methods: {
-    startTracking() {
-      window.GazeRecorderAPI.Rec();
-      console.log('started');
-    },
     stopTracking() {
-      window.GazeRecorderAPI.StopRec();
-      this.SesionReplayData = window.GazeRecorderAPI.GetRecData();
-      console.log('stopped');
+      window.GazeCloudAPI.StopEyeTracking()
     },
-    replay() {
-      window.GazePlayer.PlayResultsData(this.SesionReplayData );
+    plotGaze(GazeData) {
+      /*
+          GazeData.state // 0: valid gaze data; -1 : face tracking lost, 1 : gaze uncalibrated
+          GazeData.docX // gaze x in document coordinates
+          GazeData.docY // gaze y in document cordinates
+          GazeData.time // timestamp
+      */
+
+      var docx = GazeData.docX;
+      var docy = GazeData.docY;
+
+      var gaze = document.getElementById("gaze");
+      docx -= gaze.clientWidth / 2;
+      docy -= gaze.clientHeight / 2;
+
+      gaze.style.left = docx + "px";
+      gaze.style.top = docy + "px";
+
+
+      if (GazeData.state != 0) {
+          if (gaze.style.display == 'block')
+              gaze.style.display = 'none';
+      }
+      else {
+          if (gaze.style.display == 'none')
+              gaze.style.display = 'block';
+      }
     }
   },
+  watch: {
+    isGazeCloudApiLoaded(value) {
+      if(value) {
+        window.GazeCloudAPI.StartEyeTracking();
+        window.GazeCloudAPI.OnResult = this.plotGaze;
+      }
+    }
+  }
 }
 </script>
